62aee59b4daac27264e2c91a279a7ae97875c338
Ilham Syahid S <ilhamsyahids@gmail.com>
2020-03-15 11:59:01 +0700

    init gui

---
+++ src/ui/controller/MainWindowController.java
@@ -0,0 +1,243 @@
+package ui.controller;
+
+import ui.view.MainWindow;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainWindowController {
+    private MainWindow mainWindow;
+    private JTextField numField;
+    private JTextField errorField;
+    private JLabel label;
+    private JButton btnOne;
+    private JButton btnTwo;
+    private JButton btnDot;
+    private JButton btnZero;
+    private JButton btnAns;
+    private JButton btnPlus;
+    private JButton btnSix;
+    private JButton btnMul;
+    private JButton btnMin;
+    private JButton btnDiv;
+    private JButton btnEqu;
+    private JButton btnThree;
+    private JButton btnNine;
+    private JButton btnClear;
+    private JButton btnMemC;
+    private JButton btnSeven;
+    private JButton btnEight;
+    private JButton btnFour;
+    private JButton btnFive;
+    private JButton btnMemR;
+    private JButton btnSquare;
+
+    public MainWindowController() {
+        initComponents();
+        initListeners();
+    }
+
+    public void initComponents() {
+        mainWindow = new MainWindow();
+        btnOne = mainWindow.getBtnOne();
+        btnTwo = mainWindow.getBtnTwo();
+        btnThree = mainWindow.getBtnThree();
+        btnFour = mainWindow.getBtnFour();
+        btnFive = mainWindow.getBtnFive();
+        btnSix = mainWindow.getBtnSix();
+        btnSeven = mainWindow.getBtnSeven();
+        btnEight = mainWindow.getBanEight();
+        btnNine = mainWindow.getBtnNine();
+        btnZero = mainWindow.getBtnZero();
+        btnDot = mainWindow.getBtnDot();
+        btnPlus = mainWindow.getBtnPlus();
+        btnMin = mainWindow.getBtnMin();
+        btnMul = mainWindow.getBtnMul();
+        btnDiv = mainWindow.getBtnDiv();
+        btnSquare = mainWindow.getBtnSquare();
+        btnClear = mainWindow.getBtnClear();
+        btnAns = mainWindow.getBtnAns();
+        btnEqu = mainWindow.getBtnEqu();
+        btnMemC = mainWindow.getBtnMemC();
+        btnMemR = mainWindow.getBtnMemR();
+        numField = mainWindow.getNumField();
+        errorField = mainWindow.getErrorField();
+    }
+
+    private void initListeners() {
+        btnDot.addActionListener(new DotBtnListener());
+        btnOne.addActionListener(new OneBtnListener());
+        btnTwo.addActionListener(new TwoBtnListener());
+        btnThree.addActionListener(new ThreeBtnListener());
+        btnFour.addActionListener(new FourBtnListener());
+        btnFive.addActionListener(new FiveBtnListener());
+        btnSix.addActionListener(new SixBtnListener());
+        btnSeven.addActionListener(new SevenBtnListener());
+        btnEight.addActionListener(new EightBtnListener());
+        btnNine.addActionListener(new NineBtnListener());
+        btnZero.addActionListener(new ZeroBtnListener());
+        btnClear.addActionListener(new ClearBtnListener());
+        btnMemC.addActionListener(new MemCBtnListener());
+        btnMemR.addActionListener(new MemRBtnListener());
+        btnAns.addActionListener(new AnsBtnListener());
+        btnEqu.addActionListener(new EquBtnListener());
+        btnMul.addActionListener(new MulBtnListener());
+        btnDiv.addActionListener(new DivBtnListener());
+        btnPlus.addActionListener(new PlusBtnListener());
+        btnMin.addActionListener(new MinBtnListener());
+        btnSquare.addActionListener(new SquareBtnListener());
+    }
+
+    public void showWindow() {
+        mainWindow.setVisible(true);
+    }
+
+
+    private class AnsBtnListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    private class DotBtnListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    private class OneBtnListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            numField.setText(numField.getText() + '1');
+        }
+    }
+
+    private class TwoBtnListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            numField.setText(numField.getText() + '2');
+        }
+    }
+
+    private class ThreeBtnListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    private class FourBtnListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    private class FiveBtnListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    private class SixBtnListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    private class SevenBtnListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    private class EightBtnListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    private class NineBtnListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    private class ZeroBtnListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    private class ClearBtnListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    private class MemCBtnListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    private class MemRBtnListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    private class EquBtnListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    private class MulBtnListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    private class DivBtnListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    private class PlusBtnListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    private class MinBtnListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    private class SquareBtnListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+}
---
+++ src/ui/view/MainWindow.java
@@ -0,0 +1,134 @@
+package ui.view;
+
+import javax.swing.*;
+
+public class MainWindow extends JFrame{
+    private JTextField numField;
+    private JTextField errorField;
+    private JLabel label;
+    private JButton btnDot;
+    private JButton btnZero;
+    private JButton btnAns;
+    private JButton btnPlus;
+    private JButton btnTwo;
+    private JButton btnSix;
+    private JButton btnMul;
+    private JButton btnMin;
+    private JButton btnDiv;
+    private JButton btnEqu;
+    private JButton btnOne;
+    private JButton btnThree;
+    private JButton btnNine;
+    private JButton btnClear;
+    private JButton btnMemC;
+    private JButton btnSeven;
+    private JButton banEight;
+    private JButton btnFour;
+    private JButton btnFive;
+    private JButton btnMemR;
+    private JButton btnSquare;
+    private JPanel mainPanel;
+
+    public MainWindow() {
+        setSize(500, 500);
+        setContentPane(mainPanel);
+        setLocationRelativeTo(null);
+        setResizable(false);
+    }
+
+    public JTextField getNumField() {
+        return numField;
+    }
+
+    public JTextField getErrorField() {
+        return errorField;
+    }
+
+    public JLabel getLabel() {
+        return label;
+    }
+
+    public JButton getBtnDot() {
+        return btnDot;
+    }
+
+    public JButton getBtnZero() {
+        return btnZero;
+    }
+
+    public JButton getBtnAns() {
+        return btnAns;
+    }
+
+    public JButton getBtnPlus() {
+        return btnPlus;
+    }
+
+    public JButton getBtnTwo() {
+        return btnTwo;
+    }
+
+    public JButton getBtnSix() {
+        return btnSix;
+    }
+
+    public JButton getBtnMul() {
+        return btnMul;
+    }
+
+    public JButton getBtnMin() {
+        return btnMin;
+    }
+
+    public JButton getBtnDiv() {
+        return btnDiv;
+    }
+
+    public JButton getBtnEqu() {
+        return btnEqu;
+    }
+
+    public JButton getBtnOne() {
+        return btnOne;
+    }
+
+    public JButton getBtnThree() {
+        return btnThree;
+    }
+
+    public JButton getBtnNine() {
+        return btnNine;
+    }
+
+    public JButton getBtnClear() {
+        return btnClear;
+    }
+
+    public JButton getBtnMemC() {
+        return btnMemC;
+    }
+
+    public JButton getBtnSeven() {
+        return btnSeven;
+    }
+
+    public JButton getBanEight() {
+        return banEight;
+    }
+
+    public JButton getBtnFour() {
+        return btnFour;
+    }
+
+    public JButton getBtnFive() {
+        return btnFive;
+    }
+
+    public JButton getBtnMemR() {
+        return btnMemR;
+    }
+
+    public JButton getBtnSquare() {
+        return btnSquare;
+    }
+}

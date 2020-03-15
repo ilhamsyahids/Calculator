d46b2b42f42f9540bae8bffee6cb466b39daf402
Ilham Syahid S <ilhamsyahids@gmail.com>
2020-03-15 21:24:55 +0700

    update with .idea

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import ui.controller.MainWindowController;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         MainWindowController m = new MainWindowController();
         m.showWindow();
     }
